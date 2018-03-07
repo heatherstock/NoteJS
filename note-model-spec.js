ecb6aa4d9ede0b705c4735d5d32f2e1e6f054bd3
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-07 11:42:44 +0000

    Add first test

--- note-model-spec.js
+++ note-model-spec.js
@@ -1,3 +1,11 @@
 (function(exports) {
-  
+  function takesNotesOnInstantiation() {
+    var note = new Note();
+    if (note.text !== "test string") {
+      throw new Error("Test fails")
+    } else {
+      console.log('Test passes')
+    }
+  }
+  takesNotesOnInstantiation();
 })(this);
