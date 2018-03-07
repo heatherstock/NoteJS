3804200d159b36c150827969c3d17550d9f9fdb3
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-03-07 15:04:52 +0000

    add method to return note text, refactor test suite for better console display

--- note-model-spec.js
+++ note-model-spec.js
@@ -1,5 +1,15 @@
 function takesNotesOnInstantiation() {
+  describe('Note');
+  it('takes string as argument on instantiation');
   var note = new Note("test string");
-  expect.toBeTrue(note.text === "test string")
+  expect.toBeTrue(note.text === "test string");
 };
 takesNotesOnInstantiation();
+
+function returnsNoteText() {
+  describe('#returnNote');
+  it('returns note text');
+  var note = new Note("test string");
+  expect.toBeTrue(note.returnNote() === "test string");
+};
+returnsNoteText();
--- test-suite.js
+++ test-suite.js
@@ -4,9 +4,21 @@
       if (!assertionToCheck) {
         throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
       } else {
-        console.log("Test passes")
+        console.log("Test passes");
+        console.log(' ')
       }
     }
   };
+
+  var describe = function(string) {
+    console.log(string)
+  };
+
+  var it = function(string) {
+    console.log('     ' + string)
+  };
+
   exports.expect = expect;
+  exports.describe = describe;
+  exports.it = it;
 })(this);
