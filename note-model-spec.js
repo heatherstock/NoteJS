70b17dc48ed7c59c4011c3ead213a279e0a5d1ca
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-07 11:48:15 +0000

    Pass first test

--- note-model-spec.js
+++ note-model-spec.js
@@ -1,6 +1,6 @@
 (function(exports) {
   function takesNotesOnInstantiation() {
-    var note = new Note();
+    var note = new Note("test string");
     if (note.text !== "test string") {
       throw new Error("Test fails")
     } else {
--- note-model.js
+++ note-model.js
@@ -1,3 +1,6 @@
 (function(exports) {
-  
+  function Note(string) {
+    this.text = string;
+  };
+  exports.Note = Note;
 })(this);
