684fc0ef0ba8f2ef616123e2dfa133ff42a65295
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-08 16:31:43 +0000

    Solve return every note elements text wrapped in html;

--- note-list-view-spec.js
+++ note-list-view-spec.js
@@ -16,10 +16,12 @@ function createNoteListViewOnInstantiation() {
   var notelist = new Notelist();
   var newnote = new Note("hello");
   var newnote2 = new Note("Bye");
+  var newnote3 = new Note("yesokey")
   notelist.list.push(newnote);
   notelist.list.push(newnote2);
+  notelist.list.push(newnote3);
   var notelistview = new NoteListView(notelist);
-  var html = "<ul><li><div>hello</div></li><li><div>Bye</div></li></ul>"
+  var html = "<ul><li><div>hello</div></li><li><div>Bye</div></li><li><div>yesokey</div></li></ul>"
   expect.toBeEqual(notelistview.createNoteListView(), html);
 };
 createNoteListViewOnInstantiation();
--- note-list-view.js
+++ note-list-view.js
@@ -2,14 +2,15 @@
   function NoteListView(notelist) {
     this.notelist = notelist;
   };
+  
   NoteListView.prototype.createNoteListView = function() {
-    // var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
-    // return html;
     var array = this.notelist.getsNotes();
+    var string = "<ul>"
     for (var i = 0; i < array.length; ++i) {
-      return `<ul><li><div>${array[i].text}</div></li></ul>`;
+      string += `<li><div>${array[i].text}</div></li>`;
     }
-
+    string += "</ul>"
+    return string
   };
 
   exports.NoteListView = NoteListView;
