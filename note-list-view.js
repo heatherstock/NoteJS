0923991e16d2f3e143a7f15bcef218a1a8864bc2
Ben <ben.challenor@btopenworld.com>
2018-03-08 16:13:23 +0000

    Test for adding note to html still to pass

--- note-list-view.js
+++ note-list-view.js
@@ -5,8 +5,10 @@
   NoteListView.prototype.createNoteListView = function() {
     // var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
     // return html;
-
-    return `<ul><li><div>${this.notelist.getsNotes()[0].text}</div></li><li><div>${this.notelist.getsNotes()[1].text}</div></li></ul>`
+    var array = this.notelist.getsNotes();
+    for (var i = 0; i < array.length; ++i) {
+      return `<ul><li><div>${array[i].text}</div></li></ul>`;
+    }
 
   };
 
