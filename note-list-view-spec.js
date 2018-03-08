81a8848b8f74406caacdc98bdd34898c8181d260
Ben <ben.challenor@btopenworld.com>
2018-03-08 15:14:25 +0000

    Passing test for returning notes in a html string

--- note-list-view-spec.js
+++ note-list-view-spec.js
@@ -1,10 +1,25 @@
+// function createNoteListViewOnInstantiation() {
+//   describe('creates a note list view');
+//   it('returns a string of HTML');
+//
+//   var notelist = new Notelist();
+//   var notelistview = new NoteListView(notelist);
+//   var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
+//   expect.toBeEqual(notelistview.createNoteListView(), html);
+// };
+// createNoteListViewOnInstantiation();
+
 function createNoteListViewOnInstantiation() {
   describe('creates a note list view');
-  it('returns a string of HTML');
+  it('returns a string of HTML with notes');
 
   var notelist = new Notelist();
+  var newnote = new Note("hello");
+  var newnote2 = new Note("Bye");
+  notelist.list.push(newnote);
+  notelist.list.push(newnote2);
   var notelistview = new NoteListView(notelist);
-  var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
+  var html = "<ul><li><div>hello</div></li><li><div>Bye</div></li></ul>"
   expect.toBeEqual(notelistview.createNoteListView(), html);
 };
 createNoteListViewOnInstantiation();
--- note-list-view.js
+++ note-list-view.js
@@ -3,8 +3,11 @@
     this.notelist = notelist;
   };
   NoteListView.prototype.createNoteListView = function() {
-    var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
-    return html;
+    // var html = "<ul><li><div>EMPTY</div></li><li><div>EMPTY</div></li></ul>"
+    // return html;
+
+    return `<ul><li><div>${this.notelist.getsNotes()[0].text}</div></li><li><div>${this.notelist.getsNotes()[1].text}</div></li></ul>`
+
   };
 
   exports.NoteListView = NoteListView;
