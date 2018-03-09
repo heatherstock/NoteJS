10f7740c9b6b5c26c83106e05649b8e2148a8ed0
Ben <ben.challenor@btopenworld.com>
2018-03-09 15:41:58 +0000

    Added function for adding note to index page

--- note-controller.js
+++ note-controller.js
@@ -1,21 +1,17 @@
 (function(exports) {
   function NoteController(notelist) {
     this.notelist = notelist;
-    console.log(notelist + " line 4");
     notelist.storeNotes('Favourite drink: seltzer');
-    console.log(notelist + " line 6");
     this.noteview = new NoteListView(notelist);
-    console.log(notelist + " line 8");
+
   }
-console.log(NoteController());
+
   NoteController.prototype.getHTML = function(){
-    document.getElementById('app').innerHTML = this.noteview.createNoteListView();;
+    document.getElementById('app').innerHTML = this.noteview.createNoteListView();
   };
-console.log(NoteController.getHTML());
   exports.NoteController = NoteController;
 })(this);
 
 var notelist = new Notelist();
 var notecontroller = new NoteController(notelist);
-NoteController(notelist);
-console.log(notecontroller.getHTML());
+notecontroller.getHTML();
