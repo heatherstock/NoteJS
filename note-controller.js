1812dead140dbebef804d5736f496b8b9645584d
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-09 15:22:00 +0000

    fail printing html to index.html

--- note-controller.js
+++ note-controller.js
@@ -1,5 +1,21 @@
-function changeName(name) {
-  document.getElementById('app').innerHTML='howdy';
-  console.log(document.getElementById('app').innerHTML='howdy')
-}
-changeName();
+(function(exports) {
+  function NoteController(notelist) {
+    this.notelist = notelist;
+    console.log(notelist + " line 4");
+    notelist.storeNotes('Favourite drink: seltzer');
+    console.log(notelist + " line 6");
+    this.noteview = new NoteListView(notelist);
+    console.log(notelist + " line 8");
+  }
+console.log(NoteController());
+  NoteController.prototype.getHTML = function(){
+    document.getElementById('app').innerHTML = this.noteview.createNoteListView();;
+  };
+console.log(NoteController.getHTML());
+  exports.NoteController = NoteController;
+})(this);
+
+var notelist = new Notelist();
+var notecontroller = new NoteController(notelist);
+NoteController(notelist);
+console.log(notecontroller.getHTML());
